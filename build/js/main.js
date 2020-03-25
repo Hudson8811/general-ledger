06d52fcca73942239c97c950981b1af869e48906
db21 <you@example.com>
2020-03-25 16:16:05 +0300

    desktop-finish

--- build/js/main.js
+++ build/js/main.js
@@ -45,6 +45,10 @@ tpl:'<div class="fancybox-share"><h1>{{SHARE}}</h1><p><a class="fancybox-share__
 $(document).ready(function () {
     $("input[name='phone']").mask(" +7 (999) 999-99-99");
 
+    $('.sh-burger').click(function () {
+        $(this).toggleClass('sh-burger--active');
+        //siteHeader.toggleClass('site-header--mobile-menu-open');
+    });
 
     var $grid1 = $('.hww-block').masonry({
 
@@ -66,7 +70,10 @@ $(document).ready(function () {
         verticalSwiping: true,
         appendArrows: $('.oc-slider-controls'),
         appendDots: $('.oc-slider-controls'),
-        prevArrow: '<div class="oc-slick-prev"><svg width="18" height="9" viewBox="0 0 18 9" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17 1L10.317 6.84762C9.56296 7.50741 8.43704 7.50741 7.68299 6.84762L1 0.999999" stroke="#A0A0B1" stroke-width="2"/></svg></div>',
-        nextArrow: '<div class="oc-slick-next"><svg width="18" height="9" viewBox="0 0 18 9" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 8L7.68299 2.15238C8.43704 1.49259 9.56296 1.49259 10.317 2.15238L17 8" stroke="#555665" stroke-width="2"/></svg></div>'
+        prevArrow: '<div class="oc-slick-prev"><svg width="18" height="9" viewBox="0 0 18 9" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 8L7.68299 2.15238C8.43704 1.49259 9.56296 1.49259 10.317 2.15238L17 8" stroke="#555665" stroke-width="2"/></svg></div>',
+        nextArrow: '<div class="oc-slick-next"><svg width="18" height="9" viewBox="0 0 18 9" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17 1L10.317 6.84762C9.56296 7.50741 8.43704 7.50741 7.68299 6.84762L1 0.999999" stroke="#A0A0B1" stroke-width="2"/></svg></div>'
+    });
+    $('.ans-item-question').click(function () {
+        $(this).toggleClass('ans-item-question--opened').siblings('.ans-item-answer').slideToggle(200);
     });
 });
