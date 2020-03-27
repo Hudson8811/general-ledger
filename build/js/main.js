76861f8c5d0a68667a6cf7773a0042373a79a173
db21 <you@example.com>
2020-03-27 16:40:47 +0300

    123

--- build/js/main.js
+++ build/js/main.js
@@ -52,7 +52,7 @@ $(document).ready(function () {
 
     $('.sh-burger').click(function () {
         $(this).toggleClass('sh-burger--active');
-        //siteHeader.toggleClass('site-header--mobile-menu-open');
+        $('.sh-menu-wrap').toggleClass('sh-menu-wrap--open');
     });
 
     var $grid1;
@@ -116,7 +116,15 @@ $(document).ready(function () {
         appendArrows: $('.oc-slider-controls'),
         appendDots: $('.oc-slider-controls'),
         prevArrow: '<div class="oc-slick-prev"><svg width="18" height="9" viewBox="0 0 18 9" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 8L7.68299 2.15238C8.43704 1.49259 9.56296 1.49259 10.317 2.15238L17 8" stroke="#555665" stroke-width="2"/></svg></div>',
-        nextArrow: '<div class="oc-slick-next"><svg width="18" height="9" viewBox="0 0 18 9" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17 1L10.317 6.84762C9.56296 7.50741 8.43704 7.50741 7.68299 6.84762L1 0.999999" stroke="#A0A0B1" stroke-width="2"/></svg></div>'
+        nextArrow: '<div class="oc-slick-next"><svg width="18" height="9" viewBox="0 0 18 9" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17 1L10.317 6.84762C9.56296 7.50741 8.43704 7.50741 7.68299 6.84762L1 0.999999" stroke="#A0A0B1" stroke-width="2"/></svg></div>',
+        responsive: [
+            {
+              breakpoint: 700,
+              settings: {
+                vertical: false,
+              }
+            }
+          ]
     });
     $('.ans-item-question').click(function () {
         $(this).toggleClass('ans-item-question--opened').siblings('.ans-item-answer').slideToggle(200);
@@ -138,7 +146,7 @@ $(document).ready(function () {
 
     var wcuAndSpBlockState = 'default';//default/scrollbar/slider
 
-    function wcuAndSpInitScrollbar(params) {
+    function wcuAndSpInitScrollbar() {
         $('.wcu-block-wrap').mCustomScrollbar({
             theme: "custom-theme",
             axis: "x",
@@ -152,12 +160,31 @@ $(document).ready(function () {
 
         wcuAndSpBlockState = 'scrollbar';
     }
-    if (window.matchMedia('(max-width: 1000px)').matches) {
+    function wcuAndSpInitSlider() {
+        $('.wcu-block').slick({
+            prevArrow: '<div class="wwsy-slick-prev wwsy-slick-prev--wcu"><svg width="15" height="32" viewBox="0 0 15 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14 1L2 16L14 31" stroke="#C7C7D5" stroke-width="2"/></svg></div>',
+            nextArrow: '<div class="wwsy-slick-next wwsy-slick-next--wcu"><svg width="15" height="32" viewBox="0 0 15 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 31L13 16L1 0.999999" stroke="#C7C7D5" stroke-width="2"/></svg></div>'
+        });
+        $('.sp-block').slick({
+            prevArrow: '<div class="hww-slick-prev hww-slick-prev--sp"><svg width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 1L2 8L8 15" stroke="#555665" stroke-width="2"/></svg></div>',
+            nextArrow: '<div class="hww-slick-next hww-slick-next--sp"><svg width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 15L7 8L1 0.999999" stroke="#555665" stroke-width="2"/></svg></div>'
+        });
+
+        $(".sp-block.slick-slider").on('afterChange', function (event, slick, currentSlide) {
+            $(".js-sp-block-mobile-counter__current").text(currentSlide + 1);
+        });
+
+        wcuAndSpBlockState = 'slider';
+    }
+    if (window.matchMedia('(min-width: 701px) and (max-width: 1000px)').matches) {
         wcuAndSpInitScrollbar();
+    }
+    if (window.matchMedia('(max-width: 700px)').matches) {
+        wcuAndSpInitSlider();
     }
 
     $(window).resize(function () {
-        if (window.matchMedia('(max-width: 1000px)').matches) {
+        if (window.matchMedia('(min-width: 701px) and (max-width: 1000px)').matches) {
             if (wcuAndSpBlockState === 'default') {
                 wcuAndSpInitScrollbar();
             }
@@ -168,4 +195,61 @@ $(document).ready(function () {
             wcuAndSpBlockState = 'default';
         }
     });
+
+    ////////////////ФУТЕР
+
+    function adaptFooter() {
+        if (window.matchMedia('(max-width: 1000px)').matches) {
+            if (!$('.sf-col-3').parent().hasClass('sf-tb-container')) {
+                $('.sf-col-3').appendTo($('.sf-tb-container'));
+                $('.sf-col-5').appendTo($('.sf-tb-container'));
+            }
+        }
+        else {
+            console.log($('.sf-col-3').parent());
+            if ($('.sf-col-3').parent().hasClass('sf-tb-container')) {
+                $('.sf-col-3').appendTo($('.sf-container.container'));
+                $('.sf-col-5').appendTo($('.sf-container.container'));
+            }
+        }
+    }
+    adaptFooter();
+    $(window).resize(function () {
+        adaptFooter();
+    });
+
+
+
+
+    if (window.matchMedia('(max-width: 700px)').matches) {
+        $('.wwsy-block').slick({
+            autoplay: true,
+            autoplaySpeed: 2000,
+            prevArrow: '<div class="wwsy-slick-prev"><svg width="15" height="32" viewBox="0 0 15 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14 1L2 16L14 31" stroke="#C7C7D5" stroke-width="2"/></svg></div>',
+            nextArrow: '<div class="wwsy-slick-next"><svg width="15" height="32" viewBox="0 0 15 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 31L13 16L1 0.999999" stroke="#C7C7D5" stroke-width="2"/></svg></div>'
+        });
+        $(".wwsy-block.slick-slider").on('afterChange', function (event, slick, currentSlide) {
+            $(".js-wwsy-block-mobile-counter__current").text(currentSlide + 1);
+        });
+
+        //////////////////////////////
+
+
+
+        $('.hww-block').slick({
+            prevArrow: '<div class="hww-slick-prev"><svg width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 1L2 8L8 15" stroke="#555665" stroke-width="2"/></svg></div>',
+            nextArrow: '<div class="hww-slick-next"><svg width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 15L7 8L1 0.999999" stroke="#555665" stroke-width="2"/></svg></div>'
+        });
+        $('.hww-block-mobile-unwrap span').click(function () {
+            $('.hww-block').addClass('hww-block--shown');
+            setTimeout(function () {
+                $('.hww-block-mobile-unwrap').addClass('hww-block-mobile-unwrap--dn');
+            }, 200);
+        });
+    }
+
+    /* function wwsyBlock(params) {
+
+         wcuAndSpBlockState = 'scrollbar';
+     }*/
 });
