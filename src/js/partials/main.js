1290e3cd7fe93a8bc8d2686598cc03263119c881
ArtZ <snickgem@gmail.com>
2021-07-14 18:35:56 +0400

    why-block mobile slider

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -472,3 +472,50 @@ $(".hww-block-mobile-unwrap span").click(function () {
 $(".wwsy-uprotch").on('afterChange', function (event, slick, currentSlide) {
   $("#cp").text(currentSlide + 1);
 });
+
+function dnsSliderInit() {
+	if (window.matchMedia("(max-width: 1025px)").matches) {
+		$(".js-dns-slider").slick({
+			dots: false,
+			arrows: false,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			prevArrow: '<div class="wwsy-uprotch-arrow-prev"><svg width="11" height="22" viewBox="0 0 11 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 21L2.20414 12.3379C1.51959 11.5773 1.51959 10.4227 2.20414 9.66207L10 1" stroke="#C7C7D5" stroke-width="2"/></svg></div>',
+			nextArrow: '<div class="wwsy-uprotch-arrow-next"><svg width="11" height="22" viewBox="0 0 11 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L8.79586 9.66207C9.48041 10.4227 9.48041 11.5773 8.79586 12.3379L1 21" stroke="#C7C7D5" stroke-width="2"/></svg></div>',
+			responsive: [
+				{
+					breakpoint: 1025,
+					settings: {
+						arrows: true
+					},
+				},
+			],
+		});
+  } else if (window.matchMedia("(min-width: 1026px)").matches) {
+		$(".js-dns-slider").slick("unslick");
+  }
+}
+
+$(window).resize(function () {
+	setEqualHeight($(".js-dns-slider .dns-item"));
+	dnsSliderInit();
+});
+
+setEqualHeight($(".js-dns-slider .dns-item"));
+dnsSliderInit();
+
+function setEqualHeight(el) {
+	if (window.matchMedia("(max-width: 1025px)").matches) {
+		var tallestcolumn = 0;
+
+		el.each(function() {
+			var currentHeight = $(this).height();
+
+			if (currentHeight > tallestcolumn) {
+				tallestcolumn = currentHeight;
+			}
+		});
+
+		el.height(tallestcolumn);
+	}
+}
